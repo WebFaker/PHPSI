d3dfcf3896763fbb5bbac8e9498c3104642dad14
Nicolas MARTIN <WebFaker@github.com>
2018-02-15 19:29:10 +0100

    Added doadd.php comments

--- admin/doadd.php
+++ admin/doadd.php
@@ -3,11 +3,12 @@
 if(isset($_POST['planet']))      $planet=$_POST['planet'];
 else      $planet="";
 
+// If the user entered a value in the formula, it's creating a var with the value in it
 if(isset($_POST['img']))      $img=$_POST['img'];
+// Otherwise, if nothing has been typed, create an empty var
 else      $img="";
 
 if(isset($_POST['desc']))      $desc=$_POST['desc'];
-// If nothing has been typed, create an empty var
 else      $desc="";
 
 if(isset($_POST['temp']))      $temp=$_POST['temp'];
@@ -32,22 +33,27 @@ else
          $password = "coucou";
          $dbname = "outland";
 
-// Trying to 
+// Trying to connect
          try {
              $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
              // set the PDO error mode to exception
              $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+// Inserting into the database named "planetes", into the columns : "planete", "image", "description", "temperature" and "km"
+// The values below : "$planet", "$img", "$desc", "$temp", "$km"
              $sql = "INSERT INTO planetes (planete, image, description, temperature, km)
              VALUES ('$planet', '$img', '$desc', '$temp', '$km')";
              // use exec() because no results are returned
              $conn->exec($sql);
+// header is used to take the user to a new page, here, to success.html
              header('Location: success.html');
              }
+
+// If there's any error, use the catch part below
          catch(PDOException $e)
              {
              echo $sql . "<br>" . $e->getMessage();
              }
-
+// Exit the database
          $conn = null;
        }
          ?>
